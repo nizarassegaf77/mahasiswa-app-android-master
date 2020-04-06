eb4444aacf92a461e86b47a71203e8603b26c916
nizarassegaf <nizar.assegaf@stickearn.com>
2020-04-06 18:47:04 +0700

    Initial commit

---
+++ app/src/main/java/com/meridianid/farizdotid/mahasiswaapp/activity/LoginActivity.java
@@ -0,0 +1,134 @@
+package com.meridianid.farizdotid.mahasiswaapp.activity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.meridianid.farizdotid.mahasiswaapp.R;
+import com.meridianid.farizdotid.mahasiswaapp.util.SharedPrefManager;
+import com.meridianid.farizdotid.mahasiswaapp.util.api.BaseApiService;
+import com.meridianid.farizdotid.mahasiswaapp.util.api.UtilsApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etEmail) EditText etEmail;
+    @BindView(R.id.etPassword) EditText etPassword;
+    @BindView(R.id.btnLogin) Button btnLogin;
+    @BindView(R.id.btnRegister) Button btnRegister;
+    @BindView(R.id.text_info_product)
+    TextView txtInfoProduct;
+    ProgressDialog loading;
+
+    Context mContext;
+    BaseApiService mApiService;
+
+    SharedPrefManager sharedPrefManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        ButterKnife.bind(this);
+        mContext = this;
+        mApiService = UtilsApi.getAPIService(); // meng-init yang ada di package apihelper
+        sharedPrefManager = new SharedPrefManager(this);
+
+
+        Spannable wordtoSpan = new SpannableString("Product of Mahapatih Sibernusa Teknologi");
+        wordtoSpan.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorPrimary)), 0, 10, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        txtInfoProduct.setText(wordtoSpan);
+        txtInfoProduct.setTypeface(Typeface.DEFAULT_BOLD);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loading = ProgressDialog.show(mContext, null, "Harap Tunggu...", true, false);
+                requestLogin();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, RegisterActivity.class));
+            }
+        });
+
+        // Code berikut berfungsi untuk mengecek session, Jika session true ( sudah login )
+        // maka langsung memulai MainActivity.
+        if (sharedPrefManager.getSPSudahLogin()){
+            startActivity(new Intent(LoginActivity.this, MainActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+        }
+    }
+
+    private void requestLogin(){
+        mApiService.loginRequest(etEmail.getText().toString(), etPassword.getText().toString())
+                .enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful()){
+                            loading.dismiss();
+                            try {
+                                JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                                if (jsonRESULTS.getString("error").equals("false")){
+                                    // Jika login berhasil maka data nama yang ada di response API
+                                    // akan diparsing ke activity selanjutnya.
+                                    Toast.makeText(mContext, "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
+                                    String nama = jsonRESULTS.getJSONObject("user").getString("nama");
+                                    sharedPrefManager.saveSPString(SharedPrefManager.SP_NAMA, nama);
+                                    // Shared Pref ini berfungsi untuk menjadi trigger session login
+                                    sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, true);
+                                    startActivity(new Intent(mContext, MainActivity.class)
+                                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+                                    finish();
+                                } else {
+                                    // Jika login gagal
+                                    String error_message = jsonRESULTS.getString("error_msg");
+                                    Toast.makeText(mContext, error_message, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            loading.dismiss();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.e("debug", "onFailure: ERROR > " + t.toString());
+                        loading.dismiss();
+                    }
+                });
+    }
+}
